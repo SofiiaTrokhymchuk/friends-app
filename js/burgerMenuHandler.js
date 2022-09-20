48de53e9b2bef385ce0f182bdcf724f16fb4225b
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2022-09-20 23:08:33 +0300

    Close menu fix

--- js/burgerMenuHandler.js
+++ js/burgerMenuHandler.js
@@ -2,16 +2,18 @@ export function hamburgerMenuClickHandler(){
     const mainContainer = document.querySelector('.main');
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const inputContainer =  document.getElementById('inputContainer');
+    let menuIsOpened = false;
     hamburgerMenu.addEventListener('click', () => {
         toggleHamburgerMenuClasses();
     })
     mainContainer.addEventListener('click', (event) => {
-        if(!event.target.closest('.input-container') && !event.target.closest('.hamburger-menu')){
+        if(menuIsOpened && !event.target.closest('.input-container') && !event.target.closest('.hamburger-menu')){
             toggleHamburgerMenuClasses();
         }
     })
 
     function toggleHamburgerMenuClasses(){
+        menuIsOpened = !menuIsOpened;
         inputContainer.classList.toggle('opened');
         hamburgerMenu.classList.toggle('active');
     }
