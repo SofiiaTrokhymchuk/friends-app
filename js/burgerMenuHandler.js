0070c82af271531861dcd92ef433622f540cebf3
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2022-09-20 20:18:50 +0300

    Add close hamburger menu option

--- js/burgerMenuHandler.js
+++ js/burgerMenuHandler.js
@@ -2,7 +2,16 @@ export function hamburgerMenuClickHandler(){
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const inputContainer =  document.getElementById('inputContainer');
     hamburgerMenu.addEventListener('click', () => {
+        toggleHamburgerMenuClasses();
+    })
+    document.addEventListener('click', (event) => {
+        if(!event.target.closest('.input-container') && !event.target.closest('.hamburger-menu')){
+            toggleHamburgerMenuClasses();
+        }
+    })
+
+    function toggleHamburgerMenuClasses(){
         inputContainer.classList.toggle('opened');
         hamburgerMenu.classList.toggle('active');
-    })
+    }
 }
